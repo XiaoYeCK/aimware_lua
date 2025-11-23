9b59ad29c640d547eb2d4c85a7f6b5be0ade6c24
能大 <139523852+XiaoYeCK@users.noreply.github.com>
2025-11-23 15:11:02 +0800

    Delete 输出GUI子元素层次结构.lua

--- 输出GUI子元素层次结构.lua
+++
@@ -1,8 +0,0 @@
-local function PrintChildren(obj, prefix)
-    print(prefix .. obj:GetName());
-    for child in obj:Children() do
-        PrintChildren(child, prefix .. "\t");
-    end
-end
-
-PrintChildren(gui.Reference("MENU"), "");
